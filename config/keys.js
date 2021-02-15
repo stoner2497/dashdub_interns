314e5e1ab6d439db86ae1f79721cd212f9ecab4a
stoner2497 <sahilshah22269@gmail.com>
2021-02-15 14:56:43 +0530

    crud operations completed

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 // to use it for export files cvontent to another file
 
-
 module.exports = {
-    mongoURI:`mongodb+srv://sahil:yGRS5tkpFXrr6oXY@cluster0.0lboc.mongodb.net/<dbname>?retryWrites=true&w=majority`   
-}
+	mongoURI: `mongodb+srv://sahil:d61PDQfGjq0pOrmD@cluster0.1n5ip.mongodb.net/internship?retryWrites=true&w=majority`,
+};
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,4 +1,5 @@
 //REQUIRING EXPRESS
+const { Router } = require('express');
 const express = require('express');
 
 //creating an instance for express router
@@ -6,34 +7,79 @@ const route = express.Router();
 
 const Todo = require('../models/TodoSchema');
 
-route.get('/', (req, res) => {
-	res.send('welcome');
+route.get('/', async (req, res) => {
+	try {
+		const result = await Todo.find({});
+		if (result) {
+			console.log(result);
+		} else {
+			console.log('no result');
+		}
+	} catch (err) {
+		console.log(err);
+	}
 });
 
-route.post('/add', (req, res) => {
+route.post('/add', async (req, res) => {
+	try {
+		// access data from headers using req.body
+		let title = req.body.title;
+		let description = req.body.description;
 
-    // access data from headers using req.body
-	let title = req.body.title;
-	let description = req.body.description;
-
-
-    //create instance of todo mongo model
-	const newTodo = new Todo({
-		title: title,
-		description: description,
-	});
-
-
-    //save data using save function of mongo model instance
-	newTodo
-		.save()
-		.then((response) => {
-			console.log(response);
-			res.send(response);
-		})
-		.catch((err) => {
-			console.log(err);
+		//create instance of todo mongo model
+		const newTodo = new Todo({
+			title: title,
+			description: description,
 		});
+
+		//save data using save function of mongo model instance
+		const result = await newTodo.save();
+		if (result) {
+			console.log(result);
+		} else {
+			console.log('something went wrong');
+		}
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+route.post('/edit/:id', async (req, res) => {
+	try {
+		let title = req.body.title;
+		let description = req.body.description;
+
+		const result = await Todo.findByIdAndUpdate(
+			{ _id: req.params.id },
+			{
+				$set: {
+					title: title,
+					description: description,
+				},
+			}
+		);
+		if (result) {
+			console.log(result);
+		} else {
+			console.log('no todo found');
+		}
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+route.post('/delete/:id', async (req, res) => {
+	try {
+		const result = await Todo.findByIdAndDelete({ _id: req.params.id });
+
+		if (result) {
+			console.log(result);
+		} else {
+			console.log(result);
+		}
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = route;
